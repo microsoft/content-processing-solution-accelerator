2263b91559b3637a637fb3d66ffd1107601dad8b
Rafi-Microsoft <v-rafmd@microsoft.com>
2025-11-27 22:03:18 +0530

    fix lint issues

--- src/ContentProcessor/src/libs/azure_helper/azure_openai.py
+++ src/ContentProcessor/src/libs/azure_helper/azure_openai.py
@@ -1,15 +1,15 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import logging
 from urllib.parse import urlparse
 from helpers.azure_credential_utils import get_azure_credential
 from azure.ai.inference import ChatCompletionsClient
 
+
 def get_foundry_client(ai_services_endpoint: str) -> ChatCompletionsClient:
     parsed = urlparse(ai_services_endpoint)
     inference_endpoint = f"https://{parsed.netloc}/models"
-    
+
     credential = get_azure_credential()
 
     return ChatCompletionsClient(
